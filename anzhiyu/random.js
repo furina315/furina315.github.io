6b5b1fd54c370f7c0d6d9e9c4fd04ef892d724c1
furina315 <wybtgrw@outlook.com>
2025-08-01 18:28:22 +0800

    Site updated: 2025-08-01 18:28:21

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["p/这是一篇文章/","p/hello-world/"];function toRandomPost(){
+var posts=["p/这是一篇文章/","p/Test/","p/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
